804ba6fe753f0ad46d0e1cfd530918e7fe4b5809
MrHanSan <hans.sandbu@protonmail.com>
2019-11-27 16:06:06 +0100

    Did some writing. Code can now read models from file, and model is quaryable

---
+++ code/st-rdf/src/master/ntnu/Facts.java
@@ -0,0 +1,12 @@
+package master.ntnu;
+
+public class Facts {
+    String subject;
+    String predicate;
+    String object;
+    public Facts(String subject, String predicate, String object) {
+       this.subject = subject;
+       this.predicate = predicate;
+       this.object = object;
+    }
+}
--- code/st-rdf/src/master/ntnu/Main.java
+++ code/st-rdf/src/master/ntnu/Main.java
@@ -1,7 +1,76 @@
 package master.ntnu;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.util.FileManager;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Running...");
+        System.out.println("Starting...");
+//        readFactsFromFile();
+        querySelect();
+    }
+
+    private static void readFactsFromFile() {
+        System.out.println("Read from file...");
+        String file = "testData.ttl";
+        FileManager.get().addLocatorClassLoader(Main.class.getClassLoader());
+        Model model = FileManager.get().loadModel(file, null, "TURTLE");
+
+        StmtIterator iter = model.listStatements();
+        try {
+            while ( iter.hasNext() ) {
+                Statement stmt = iter.next();
+
+                Resource s = stmt.getSubject();
+                Resource p = stmt.getPredicate();
+                RDFNode o = stmt.getObject();
+
+                if ( s.isURIResource() ) {
+                    System.out.print("URI");
+                } else if ( s.isAnon() ) {
+                    System.out.print("blank");
+                }
+
+                if ( p.isURIResource() )
+                    System.out.print(" URI ");
+
+                if ( o.isURIResource() ) {
+                    System.out.print("URI");
+                } else if ( o.isAnon() ) {
+                    System.out.print("blank");
+                } else if ( o.isLiteral() ) {
+                    System.out.print("literal");
+                }
+                System.out.println();
+                System.out.println(stmt);
+
+                System.out.println();
+                System.out.println();
+            }
+        } finally {
+            if ( iter != null ) iter.close();
+        }
+    }
+
+    private static void querySelect() {
+        String file = "testData.ttl";
+        Model model = FileManager.get().loadModel(file, null, "TURTLE");
+        String queryString = "PREFIX yago:<http://yago-knowledge.org/resource/> SELECT * WHERE { ?s yago:isLocatedIn ?x}";
+        Query query = QueryFactory.create(queryString);
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+            ResultSet results = qexec.execSelect() ;
+            for ( ; results.hasNext() ; )
+            {
+                QuerySolution soln = results.nextSolution() ;
+                RDFNode x = soln.get("x") ;       // Get a result variable by name.
+//                Resource r = soln.getResource("VarR") ; // Get a result variable - must be a resource
+//                Literal l = soln.getLiteral("VarL") ;   // Get a result variable - must be a literal
+                System.out.println(x);
+                System.out.println(soln);
+            }
+        }
     }
 }
