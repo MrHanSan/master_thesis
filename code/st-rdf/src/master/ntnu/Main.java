f47d3b4bc5f686f3621a7f006febfd1e21c5631f
MrHanSan <hans.sandbu@protonmail.com>
2020-02-03 20:54:32 +0100

    Wrote some more? And experimented with DFS traversal"

--- code/st-rdf/src/master/ntnu/Main.java
+++ code/st-rdf/src/master/ntnu/Main.java
@@ -1,76 +1,125 @@
 package master.ntnu;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
-import org.apache.jena.util.FileManager;
+import org.apache.jena.tdb.TDBFactory;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting...");
-//        readFactsFromFile();
-        querySelect();
+
+        String directory = "../../yago/db" ;
+        Dataset dataset = TDBFactory.createDataset(directory);
+        dataset.begin(ReadWrite.READ);
+        Model model = dataset.getDefaultModel();
+
+        traverseStart(model, null);
+
+        dataset.end();
     }
 
-    private static void readFactsFromFile() {
-        System.out.println("Read from file...");
-        String file = "testData.ttl";
-        FileManager.get().addLocatorClassLoader(Main.class.getClassLoader());
-        Model model = FileManager.get().loadModel(file, null, "TURTLE");
 
-        StmtIterator iter = model.listStatements();
-        try {
-            while ( iter.hasNext() ) {
-                Statement stmt = iter.next();
+    private static List<String> getRoots(Model model) {
+        List<String> roots = new ArrayList<String>();
+        final String RESOURCE = "http://yago-knowledge.org/resource/";
+        String queryString =
+                "PREFIX yago:<" + RESOURCE + "> " +
+                "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema> " +
+//                "SELECT ?s ?p ?o WHERE { ?s <"+RESOURCE+"isLocatedIn> ?o .FILTER regex(str(?s), \"Oslo\") " +
+//                        "FILTER ( ?s = <"+RESOURCE+"isLocatedIn> ) " +
+//                "SELECT ?s ?p ?o WHERE { ?s ?p ?o .FILTER regex(str(?s), \"Oslo\") " +
+                "SELECT DISTINCT ?s WHERE { ?s ?p <" + RESOURCE + "Oslo> . " +
+//                "SELECT ?s { { ?s yago:isLocatedIn <" + RESOURCE + "Oslo> . } " +
+//                        "UNION { ?s yago:isConnectedTo <" + RESOURCE + "Oslo> . } " +
+//                        "UNION { ?s yago:isLeaderOf <" + RESOURCE + "Oslo> . } " +
+//                        "UNION { ?s yago:happenedIn <" + RESOURCE + "Oslo> . } " +
+//                        "UNION { ?s yago:wasBornIn <" + RESOURCE + "Oslo> . } " +
+//                        "UNION { ?s yago:diedIn <" + RESOURCE + "Oslo> . } " +
+//                        "UNION { ?s yago:hasCapital <" + RESOURCE + "Oslo> . } " +
+//                        "UNION { ?s yago:livesIn <" + RESOURCE + "Oslo> . } " +
+                "}";
 
-                Resource s = stmt.getSubject();
-                Resource p = stmt.getPredicate();
-                RDFNode o = stmt.getObject();
-
-                if ( s.isURIResource() ) {
-                    System.out.print("URI");
-                } else if ( s.isAnon() ) {
-                    System.out.print("blank");
-                }
-
-                if ( p.isURIResource() )
-                    System.out.print(" URI ");
-
-                if ( o.isURIResource() ) {
-                    System.out.print("URI");
-                } else if ( o.isAnon() ) {
-                    System.out.print("blank");
-                } else if ( o.isLiteral() ) {
-                    System.out.print("literal");
-                }
-                System.out.println();
-                System.out.println(stmt);
-
-                System.out.println();
-                System.out.println();
-            }
-        } finally {
-            if ( iter != null ) iter.close();
-        }
-    }
-
-    private static void querySelect() {
-        String file = "testData.ttl";
-        Model model = FileManager.get().loadModel(file, null, "TURTLE");
-        String queryString = "PREFIX yago:<http://yago-knowledge.org/resource/> SELECT * WHERE { ?s yago:isLocatedIn ?x}";
         Query query = QueryFactory.create(queryString);
-        try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
-            ResultSet results = qexec.execSelect() ;
-            for ( ; results.hasNext() ; )
+
+        try ( QueryExecution qexec = QueryExecutionFactory.create( query, model ) ) {
+            ResultSet results = qexec.execSelect();
+            while( results.hasNext() )
             {
-                QuerySolution soln = results.nextSolution() ;
-                RDFNode x = soln.get("x") ;       // Get a result variable by name.
-//                Resource r = soln.getResource("VarR") ; // Get a result variable - must be a resource
-//                Literal l = soln.getLiteral("VarL") ;   // Get a result variable - must be a literal
-                System.out.println(x);
+                QuerySolution soln = results.nextSolution();
+                RDFNode sub = soln.get("s");
+
+                if( !sub.isURIResource() ) continue;
+
+                roots.add( sub.toString() );
                 System.out.println(soln);
             }
         }
+        return roots;
+    }
+
+
+    public static void traverseStart( Model model, String entity) {
+        // if starting class available
+        if( entity != null ) {
+            traverse( model, entity,  new ArrayList<String>(), 0);
+        }
+        // get roots and traverse each root
+        else {
+            List<String> roots = getRoots( model );
+
+            for( int i = 0; i < roots.size(); i++ ) {
+                traverse( model, roots.get( i ), new ArrayList<String>(), 0);
+            }
+        }
+    }
+
+
+    public static int traverse( Model model, String entity, List<String> occurs, int depth) {
+        if (entity == null  || depth > 2) return 99;
+        if (entity.contains("<") || entity.contains(">") || entity.contains("\"") || entity.contains(" ")) return 99;
+        int score = 0;
+
+        String queryString 	= "SELECT ?s WHERE { "
+                + "?s ?p <" + entity + "> . }";
+//        + "?s <http://www.w3.org/2000/01/rdf-schema#subClassOf> <" + entity + "> . }";
+
+//        System.out.println(queryString);
+        Query query = QueryFactory.create(queryString);
+
+        if (!occurs.contains(entity))
+        {
+            // print depth times "\t" to retrieve an explorer tree like output
+            for (int i = 0; i < depth; i++) { System.out.print("\t"); }
+            // print out the URI
+            System.out.println(entity);
+
+            try (QueryExecution qexec = QueryExecutionFactory.create( query, model ) ) {
+                ResultSet results = qexec.execSelect();
+                while (results.hasNext()) {
+                    QuerySolution soln = results.nextSolution();
+                    RDFNode sub = soln.get("s");
+
+                    if (sub == null || !sub.isURIResource()) continue;
+
+                    String str = sub.toString();
+//                    if str.contains("")
+
+                    // push this expression on the occurs list before we recurse to avoid loops
+                    occurs.add( entity );
+                    // traverse down and increase depth (used for logging tabs)
+                    traverse( model, str, occurs, depth + 1);
+                    // after traversing the path, remove from occurs list
+                    occurs.remove( entity );
+                }
+            }
+            catch (QueryParseException e) {
+                return 99;
+            }
+        }
+        return score;
     }
 }
